a04ce46e513240b8ea2bfdb42e4ddeffe215e582
Marcos <marcosrodriguesdecarvalho@gmail.com>
2014-12-27 16:05:03 -0200

    Matriz

--- matriz.py
+++ matriz.py
@@ -4,3 +4,5 @@ M=[[0,1], [2,3], [4,5]]
 condicao1 = [5,1] not in [x for x in M]
 condicao2 = [0,1] in [x for x in M]
 print (condicao1, condicao2)
+
+# Onde [x for x in M] percorre todos os elementos da matriz M
